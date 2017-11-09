594d8febcfd14430d6e6895a1ab1d516e8eb078c
josefsandoval <joeyfsandoval@gmail.com>
2017-11-08 23:49:07 -0800

    added some unit tests

--- flex/app/tests.py
+++ flex/app/tests.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import UserProfile
+from django.test import Client
 
-# Create your tests here.
+
+class UserProfileTests(TestCase):
+
+    # Test method that returns Users age
+    def test_user_correct_age(self):
+        birthday = datetime.datetime(1990,11,9)
+        user = UserProfile(date_of_birth=birthday)
+        self.assertIs(user.user_age(), 26, 'User age was not calculated correctly')
+
+
+class UserTests(TestCase):
+
+    def test_user_creation(self):
+        new_user = User.objects.create_user('newuser',None,None)
+        other_user = User.objects.create_user('otheruser',None,None)
+
+        queryset = User.objects.all()
+        self.assertQuerysetEqual(queryset, ["<User: newuser>", "<User: otheruser>"], ordered=False)
+
+    # Test if creating a user creates a UserProfile Object
+    def test_user_profile_creation(self):
+        user = User.objects.create_user('user', None, None)
+        self.assertIsNotNone(user.userprofile, 'User profile was not created along with user')
+
+
+class AuthenticationTest(TestCase):
+    # Test Django's Authentication System for logging in users
+    def test_client_login(self):
+        user = User.objects.create_user(username='jon', password='secret')
+        c = Client()
+        logged_in = c.login(username='jon', password='secret')
+        self.assertTrue(logged_in)
